01f6e1737d27f66c8822d83f5c17b29f71bc9185
dafei9522 <1047225269@qq.com>
2021-02-12 18:56:25 +0800

    init

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './index.less'
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="clorrr">
+        1111111
+      </div>
+    )
+  }
+}
